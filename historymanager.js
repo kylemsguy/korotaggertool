69f037c3428702e50ebe2f88142cc510f69611bd
Kyle Zhou <kylezhou2002@hotmail.com>
2023-09-25 15:32:31 -0700

    Implement ctrl-z and ctrl-shift-z and fix history stack weirdness

--- historymanager.js
+++ historymanager.js
@@ -6,22 +6,22 @@ const history = [];
 let currentHistoryIndex = -1;
 
 function addToHistory(tags) {
-    const newState = JSON.parse(JSON.stringify(tags));
-    history.length = currentHistoryIndex + 1;
+    const newState = JSON.stringify(tags);
+    history.length = currentHistoryIndex + 1
     history.push(newState);
     currentHistoryIndex = history.length - 1;
 }
 
 function goBackInHistory() {
     if (currentHistoryIndex > 0) {
-        return history[--currentHistoryIndex];
+        return JSON.parse(history[--currentHistoryIndex]);
     }
     return null;
 }
 
 function goForwardInHistory() {
     if (currentHistoryIndex + 1 < history.length) {
-        return history[++currentHistoryIndex];
+        return JSON.parse(history[++currentHistoryIndex]);
     }
     return null;
 }
